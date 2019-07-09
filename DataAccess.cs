87273bdf5bf9a6239ed7cd819afb81713e8b3be4
divyameher <divyaramesh1594@gmail.com>
2019-07-09 13:06:59 +0530

    Addin IRepository to perform some crud operations
    Find,Delete,Add

---
+++ DataAccess.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        T FindById(int id);
+        IQueryable<T> FindAll();
+        int Commit();
+    }
+
+    public class SqlRepository<T> : IRepository<T> where T : class
+    {
+        DbContext _context;
+        DbSet<T> _set;
+        public SqlRepository(DbContext ctx)
+        {
+            _context = ctx;
+            _set = _context.Set<T>();
+        }
+
+        public void Add(T newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
