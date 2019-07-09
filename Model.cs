43f192fbaceb363f366e8b9b6ae2fa05a0249aa2
divyameher <divyaramesh1594@gmail.com>
2019-07-09 12:47:41 +0530

    Model Objects - Perosn, Employeee and Manager

--- Model.cs
+++ Model.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace QueryIt
 {
-    class Model
+    public class Person
     {
+        public string Name { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        public virtual void DoWork()
+        {
+            Console.WriteLine("Doing Real Work!");
+        }
+    }
+    public class Manager : Employee
+    {
+        public int MyProperty { get; set; }
+        public override void DoWork()
+        {
+            Console.WriteLine("Create a Meeting");
+        }
     }
 }
